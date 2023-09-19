c52f9250f8f4e8805e95c6965a1925d4b7934106
KoreanFubuki <145092552+KoreanFubuki@users.noreply.github.com>
2023-09-19 16:52:50 +0400

    Add project files.

---
+++ Exercise 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void NestedCheck(int number)
+        {
+            if (number % 3 == 0)
+            {
+                Console.WriteLine("Divisible by 3");
+            }
+            else if (number % 7 == 0)
+            {
+                Console.WriteLine("Divisible by 7");
+            }
+            else
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("Even Number");
+                }
+                else
+                {
+                    Console.WriteLine("Odd Number");
+                }
+            }   
+        }
+        static void Main(string [] args)
+        {
+            Console.WriteLine("Please Enter a number");
+            int number = int.Parse(Console.ReadLine());
+            NestedCheck(number);
+            Console.ReadLine();
+        }
+    }
+}
